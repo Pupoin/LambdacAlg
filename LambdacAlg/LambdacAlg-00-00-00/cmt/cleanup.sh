7e74a62b47c13c26e8248f2d0cc98d82ee194764
Zhaoyang Yuan <yuanchy8@mail2.sysu.edu.cn>
2021-04-30 00:40:47 +0800

    ol

--- LambdacAlg/LambdacAlg-00-00-00/cmt/cleanup.sh
+++ LambdacAlg/LambdacAlg-00-00-00/cmt/cleanup.sh
@@ -1,4 +1,4 @@
-# echo "cleanup LambdacAlg LambdacAlg-00-00-00 in /publicfs/ucas/user/yuanchy8/workbefs/7.0.6/LambdacAlgtmp"
+# echo "cleanup LambdacAlg LambdacAlg-00-00-00 in /publicfs/ucas/user/yuanchy8/workbefs/7.0.6/LambdacAlg-etaprime"
 
 if test "${CMTROOT}" = ""; then
   CMTROOT=/cvmfs/bes3.ihep.ac.cn/bes3sw/ExternalLib/SLC6/contrib/CMT/v1r25; export CMTROOT
@@ -6,9 +6,9 @@ fi
 . ${CMTROOT}/mgr/setup.sh
 cmtLambdacAlgtempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
 if test ! $? = 0 ; then cmtLambdacAlgtempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=LambdacAlg -version=LambdacAlg-00-00-00 -path=/publicfs/ucas/user/yuanchy8/workbefs/7.0.6/LambdacAlgtmp  $* >${cmtLambdacAlgtempfile}
+${CMTROOT}/mgr/cmt cleanup -sh -pack=LambdacAlg -version=LambdacAlg-00-00-00 -path=/publicfs/ucas/user/yuanchy8/workbefs/7.0.6/LambdacAlg-etaprime  $* >${cmtLambdacAlgtempfile}
 if test $? != 0 ; then
-  echo >&2 "${CMTROOT}/mgr/cmt cleanup -sh -pack=LambdacAlg -version=LambdacAlg-00-00-00 -path=/publicfs/ucas/user/yuanchy8/workbefs/7.0.6/LambdacAlgtmp  $* >${cmtLambdacAlgtempfile}"
+  echo >&2 "${CMTROOT}/mgr/cmt cleanup -sh -pack=LambdacAlg -version=LambdacAlg-00-00-00 -path=/publicfs/ucas/user/yuanchy8/workbefs/7.0.6/LambdacAlg-etaprime  $* >${cmtLambdacAlgtempfile}"
   cmtcleanupstatus=2
   /bin/rm -f ${cmtLambdacAlgtempfile}
   unset cmtLambdacAlgtempfile
